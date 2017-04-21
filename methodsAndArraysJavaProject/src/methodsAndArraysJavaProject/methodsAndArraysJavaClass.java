734ad8b138ee218aa3956ce79da752a109ef3b4d
LAB <LAB>
2017-04-21 10:40:49 -0500

    added sum array function

--- methodsAndArraysJavaProject/src/methodsAndArraysJavaProject/methodsAndArraysJavaClass.java
+++ methodsAndArraysJavaProject/src/methodsAndArraysJavaProject/methodsAndArraysJavaClass.java
@@ -24,6 +24,16 @@ public class methodsAndArraysJavaClass {
 
 	}
 
+	private static int cmSumArray(int[] arrayOfNumbers) {
+		int arraySum = 0;
+		for(int i = 0; i < arrayOfNumbers.length; i++)
+		{
+			arraySum = arraySum + arrayOfNumbers[i];
+		}
+		
+		return 0;
+	}
+
 	private static void cmFillArray(int[] arrayOfNumbers) {
 		for(int i = 0; i < arrayOfNumbers.length; i++)
 		{
