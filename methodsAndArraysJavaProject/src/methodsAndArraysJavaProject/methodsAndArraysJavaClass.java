586db0c586e42824a499ef60194e4f9ee5bbec52
Jeremy Evert <jeremy-evert@users.noreply.github.com>
2017-04-21 10:15:19 -0500

    add hello world.

--- methodsAndArraysJavaProject/src/methodsAndArraysJavaProject/methodsAndArraysJavaClass.java
+++ methodsAndArraysJavaProject/src/methodsAndArraysJavaProject/methodsAndArraysJavaClass.java
@@ -3,7 +3,7 @@ package methodsAndArraysJavaProject;
 public class methodsAndArraysJavaClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Class.");
 
 	}
 
