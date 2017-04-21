b1e4cb50fbfbe1e717ea80ae585e015ea14fa03b
LAB <LAB>
2017-04-21 10:17:43 -0500

    added an outline of my code.

--- methodsAndArraysJavaProject/src/methodsAndArraysJavaProject/methodsAndArraysJavaClass.java
+++ methodsAndArraysJavaProject/src/methodsAndArraysJavaProject/methodsAndArraysJavaClass.java
@@ -4,6 +4,16 @@ public class methodsAndArraysJavaClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Class.");
+		
+		// Enter number of numbers sum
+		
+		// custom method to fill the array with these numbers.
+		
+		// custom method to sum the array.
+		
+		// custom method to print the sum
+		
+		// custom method to check the sum with a for loop.
 
 	}
 
